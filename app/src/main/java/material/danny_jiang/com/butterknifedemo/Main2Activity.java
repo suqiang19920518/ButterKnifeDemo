218a2651c045214a7af82fa9eb39cc3e953860bf
suqiang <1466535694@qq.com>
2016-12-18 15:07:23 +0800

    增加输出

--- app/src/main/java/material/danny_jiang/com/butterknifedemo/Main2Activity.java
+++ app/src/main/java/material/danny_jiang/com/butterknifedemo/Main2Activity.java
@@ -2,6 +2,7 @@ package material.danny_jiang.com.butterknifedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.e("d", "onCreate: " );
     }
 }
